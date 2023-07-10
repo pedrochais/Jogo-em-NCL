587604fa172cca6de7cd27aad87083a43c8b53d8
Pedro Rocha <pedrorocha.bou@gmail.com>
2023-07-10 16:13:56 -0300

    correção de bugs e fase 7

--- game.lua
+++ game.lua
@@ -8,67 +8,160 @@ local altura_tela = 500
 -- Tamanho da área de deslocamento do jogador (em pixels)
 local deslocamento = 50
 
--- Objetos no mapa 1
-local mapa_1 = {
-	{ 0, 1, 0, 1, 1, 1, 0, 1, 1, 1 },
-	{ 0, 1, 0, 0, 0, 1, 0, 0, 0, 0 },
-	{ 0, 1, 0, 1, 0, 1, 0, 1, 1, 0 },
-	{ 0, 1, 0, 1, 0, 1, 0, 1, 0, 0 },
-	{ 0, 1, 0, 1, 0, 1, 1, 1, 0, 1 },
-	{ 0, 1, 0, 1, 0, 0, 0, 0, 0, 0 },
-	{ 0, 1, 0, 1, 0, 1, 1, 0, 1, 0 },
-	{ 0, 0, 0, 1, 0, 1, 1, 0, 1, 0 },
-	{ 0, 1, 1, 1, 0, 0, 0, 0, 1, 0 },
-	{ 1, 1, 0, 0, 0, 1, 1, 0, 1, 0 },
+-- Cordenadas do jogador
+local cordenadas_jogador = {
+	{ 0, 0 },  -- Fase 1
+	{ 0, 0 },  -- Fase 2
+	{ 0, 0 },  -- Fase 3
+	{ 0, 0 },  -- Fase 4
+	{ 0, 0 },  -- Fase 5
+	{ 4, 4 },  -- Fase 6
+	{1, 9},  -- Fase 7
+	{10, 10},  -- Fim
 }
 
--- Objetos no mapa 2
-local mapa_2 = {
-	{ 0, 1, 0, 0, 0, 1, 0, 0, 0, 1 },
-	{ 0, 1, 0, 1, 0, 1, 0, 1, 0, 1 },
-	{ 0, 1, 0, 1, 0, 1, 0, 1, 0, 1 },
-	{ 0, 1, 0, 1, 0, 1, 0, 1, 0, 1 },
-	{ 0, 1, 0, 1, 0, 1, 0, 1, 0, 1 },
-	{ 0, 1, 0, 1, 0, 1, 0, 1, 0, 1 },
-	{ 0, 1, 0, 1, 0, 1, 0, 1, 0, 1 },
-	{ 0, 1, 0, 1, 0, 1, 0, 1, 0, 1 },
-	{ 0, 1, 0, 1, 0, 1, 0, 1, 0, 1 },
-	{ 0, 0, 0, 1, 0, 0, 0, 1, 0, 0 },
+-- Cordenadas da chegada
+local cordenadas_chegada = {
+	{ 9, 9 },  -- Fase 1
+	{ 9, 9 },  -- Fase 2 
+	{ 5, 5 },  -- Fase 3
+	{ 9, 9 },  -- Fase 4
+	{ 9, 9 },  -- Fase 5
+	{ 4, 9 },  -- Fase 6 
+	{9, 0},  -- Fase 7
+	{10, 10},  -- Fim
 }
 
--- Objetos no mapa 3
-local mapa_3 = {
-	{ 0, 1, 0, 0, 0, 0, 0, 0, 0, 0 },
-	{ 0, 1, 0, 1, 1, 1, 1, 1, 1, 0 },
-	{ 0, 1, 0, 1, 0, 0, 0, 0, 1, 0 },
-	{ 0, 1, 0, 1, 0, 1, 1, 0, 1, 0 },
-	{ 0, 1, 0, 1, 0, 1, 1, 0, 1, 0 },
-	{ 0, 1, 0, 1, 0, 0, 1, 0, 1, 0 },
-	{ 0, 1, 0, 1, 1, 1, 1, 0, 1, 0 },
-	{ 0, 1, 0, 0, 0, 0, 0, 0, 1, 0 },
-	{ 0, 1, 1, 1, 1, 1, 1, 1, 1, 0 },
-	{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+-- Cordenadas dos obstáculos
+local cordenadas_obstaculos = {
+	{ -- Fase 1
+		{ 0, 1, 0, 1, 1, 1, 0, 1, 1, 1 },
+		{ 0, 1, 0, 0, 0, 1, 0, 0, 0, 0 },
+		{ 0, 1, 0, 1, 0, 1, 0, 1, 1, 0 },
+		{ 0, 1, 0, 1, 0, 1, 0, 1, 0, 0 },
+		{ 0, 1, 0, 1, 0, 1, 1, 1, 0, 1 },
+		{ 0, 1, 0, 1, 0, 0, 0, 0, 0, 0 },
+		{ 0, 1, 0, 1, 0, 1, 1, 0, 1, 0 },
+		{ 0, 0, 0, 1, 0, 1, 1, 0, 1, 0 },
+		{ 1, 1, 1, 1, 0, 0, 0, 0, 1, 0 },
+		{ 0, 0, 0, 0, 0, 1, 1, 0, 1, 0 },
+	},
+	{ -- Fase 2
+		{ 0, 1, 0, 0, 0, 1, 0, 0, 0, 1 },
+		{ 0, 1, 0, 1, 0, 1, 0, 1, 0, 1 },
+		{ 0, 1, 0, 1, 0, 1, 0, 1, 0, 1 },
+		{ 0, 1, 0, 1, 0, 1, 0, 1, 0, 1 },
+		{ 0, 1, 0, 1, 0, 1, 0, 1, 0, 1 },
+		{ 0, 1, 0, 1, 0, 1, 0, 1, 0, 1 },
+		{ 0, 1, 0, 1, 0, 1, 0, 1, 0, 1 },
+		{ 0, 1, 0, 1, 0, 1, 0, 1, 0, 1 },
+		{ 0, 1, 0, 1, 0, 1, 0, 1, 0, 1 },
+		{ 0, 0, 0, 1, 0, 0, 0, 1, 0, 0 },
+	},
+	{ -- Fase 3
+		{ 0, 1, 0, 0, 0, 0, 0, 0, 0, 0 },
+		{ 0, 1, 0, 1, 1, 1, 1, 1, 1, 0 },
+		{ 0, 1, 0, 1, 0, 0, 0, 0, 1, 0 },
+		{ 0, 1, 0, 1, 0, 1, 1, 0, 1, 0 },
+		{ 0, 1, 0, 1, 0, 1, 1, 0, 1, 0 },
+		{ 0, 1, 0, 1, 0, 0, 1, 0, 1, 0 },
+		{ 0, 1, 0, 1, 1, 1, 1, 0, 1, 0 },
+		{ 0, 1, 0, 0, 0, 0, 0, 0, 1, 0 },
+		{ 0, 1, 1, 1, 1, 1, 1, 1, 1, 0 },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+	},
+	{ -- Fase 4
+		{ 0, 1, 0, 1, 0, 1, 0, 1, 0, 1 },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+		{ 1, 0, 1, 0, 1, 0, 1, 0, 1, 0 },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+		{ 0, 1, 0, 1, 0, 1, 0, 1, 0, 1 },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+		{ 1, 0, 1, 0, 1, 0, 1, 0, 1, 0 },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+		{ 0, 1, 0, 1, 0, 1, 0, 1, 0, 1 },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+	},
+	{ -- Fase 5
+		{ 0, 1, 1, 1, 0, 0, 1, 1, 1, 0 },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+		{ 0, 0, 1, 1, 0, 0, 1, 1, 0, 0 },
+		{ 0, 0, 1, 1, 0, 0, 1, 1, 0, 0 },
+		{ 0, 0, 1, 1, 0, 0, 1, 1, 0, 0 },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+		{ 0, 1, 0, 0, 0, 0, 0, 0, 1, 0 },
+		{ 0, 0, 1, 1, 1, 1, 1, 1, 0, 0 },
+		{ 0, 0, 0, 1, 1, 1, 1, 0, 0, 0 },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+	},
+	{ -- Fase 6
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+		{ 0, 1, 1, 1, 1, 0, 1, 1, 1, 0 },
+		{ 0, 1, 0, 0, 0, 0, 0, 0, 1, 0 },
+		{ 0, 1, 0, 1, 1, 1, 1, 0, 1, 0 },
+		{ 0, 1, 0, 1, 0, 0, 1, 0, 1, 0 },
+		{ 0, 1, 0, 1, 0, 0, 1, 0, 1, 0 },
+		{ 0, 1, 0, 1, 0, 1, 1, 0, 1, 0 },
+		{ 0, 1, 0, 0, 0, 0, 0, 0, 1, 0 },
+		{ 0, 1, 1, 1, 1, 1, 1, 1, 1, 0 },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+	},
+	{ -- Fase 7
+		{ 0, 0, 1, 0, 0, 0, 0, 0, 1, 0 },
+		{ 0, 0, 0, 1, 0, 0, 1, 0, 0, 0 },
+		{ 0, 1, 0, 0, 1, 0, 0, 1, 0, 0 },
+		{ 0, 0, 1, 0, 0, 1, 0, 0, 1, 0 },
+		{ 1, 0, 0, 1, 0, 0, 1, 0, 0, 1 },
+		{ 0, 1, 0, 0, 1, 0, 0, 1, 0, 0 },
+		{ 0, 0, 1, 0, 0, 1, 0, 0, 1, 0 },
+		{ 0, 0, 0, 1, 0, 0, 1, 0, 0, 0 },
+		{ 0, 1, 0, 0, 1, 0, 0, 1, 0, 0 },
+		{ 0, 0, 1, 0, 0, 0, 0, 0, 1, 0 },
+	},
+	{ -- Fim
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+	},
 }
 
 -- JOGADOR: guarda a imagem, posicao inicial e dimensoes
-local img = canvas:new('jogador.png')
-local dx, dy = img:attrSize()
-local jogador = { img = img, x = 0 * bloco, y = 0 * bloco, dx = dx, dy = dy }
+local img_jogador = canvas:new('jogador.png')
+local dx_jogador, dy_jogador = img_jogador:attrSize()
+local jogador = { img = img_jogador, x = cordenadas_jogador[fase][1]*bloco, y = cordenadas_jogador[fase][2]*bloco, dx = dx_jogador, dy = dy_jogador }
 
 -- CHEGADA: guarda a imagem, posicao inicial e dimensoes
-local img = canvas:new('chegada.png')
-local dx, dy = img:attrSize()
-local chegada = { img = img, x = 450, y = 450, dx = dx, dy = dy }
+local img_chegada = canvas:new('chegada.png')
+local dx_chegada, dy_chegada = img_chegada:attrSize()
+local chegada = { img = img_chegada, x = cordenadas_chegada[fase][1]*bloco, y = cordenadas_chegada[fase][2]*bloco, dx = dx_chegada, dy = dy_chegada }
 
 -- OBSTÁCULOS: guarda a imagem, posicao inicial e dimensoes
-local img = canvas:new('obstaculo.png')
-local dx, dy = img:attrSize()
+local img_obstaculo = canvas:new('obstaculo.png')
+local dx_obstaculo, dy_obstaculo = img_obstaculo:attrSize()
 
 local obstaculos = {}
 
+
+function atualizarCordenadas()
+	-- Atualiza as cordenadas do jogador
+	jogador.x = cordenadas_jogador[fase][1] * bloco
+	jogador.y = cordenadas_jogador[fase][2] * bloco
+
+	-- Atualiza as cordenadas da chegada
+	chegada.x = cordenadas_chegada[fase][1] * bloco
+	chegada.y = cordenadas_chegada[fase][2] * bloco
+end
+
 -- Função para gerar coordenadas a partir de uma matriz
 function gerarCoordenadas(matriz)
-	local coordenadas = {}
+	local cordenadas = {}
 
 	-- Itera sobre as linhas da matriz
 	for i = 1, #matriz do
@@ -77,55 +170,87 @@ function gerarCoordenadas(matriz)
 			-- Verifica se o valor na posição [i][j] é igual a 1
 			if matriz[i][j] == 1 then
 				-- Adiciona as coordenadas (i, j) na tabela de coordenadas
-				table.insert(coordenadas, { i, j })
+				table.insert(cordenadas, { i, j })
 			end
 		end
 	end
 
-	return coordenadas
+	return cordenadas
 end
 
-function gerarObjetos()
-	local cordenadas = {}
 
-	if fase == 1 then
-		cordenadas = gerarCoordenadas(mapa_1)
-	elseif fase == 2 then
-		cordenadas = gerarCoordenadas(mapa_2)
-	elseif fase == 3 then
-		cordenadas = gerarCoordenadas(mapa_3)
-	end
+
+
+
+function gerarObjetos()
+	-- Gera o mapa através da matriz de acordo com a fase atual
+	local cordenadas = gerarCoordenadas(cordenadas_obstaculos[fase])
+	-- Reinicializa a tabela de cordenada de obstáculos
+	obstaculos = {}
+
+	-- Desenha o jogador
+	canvas:compose(jogador.x, jogador.y, jogador.img)
 
 	-- Desenha a chegada
 	canvas:compose(chegada.x, chegada.y, chegada.img)
-	-- Desenha o jogador
-	canvas:compose(jogador.x, jogador.y, jogador.img)
+
 	-- Desenha os obstáculos
 	for i = 1, #cordenadas do
-		obstaculos[i] = { img = img, x = (cordenadas[i][2] - 1) * bloco, y = (cordenadas[i][1] - 1) * bloco, dx = dx, dy = dy }
+		obstaculos[i] = {
+			img = img_obstaculo,
+			x = (cordenadas[i][2] - 1) * bloco,
+			y = (cordenadas[i][1] - 1) * bloco,
+			dx = dx_obstaculo,
+			dy = dy_obstaculo
+		}
+		canvas:compose(obstaculos[i].x, obstaculos[i].y, obstaculos[i].img)
 	end
+end
+
+
+
+
+
+
+function gerarTexto(texto, tamanho, cor, x, y)
+	canvas:attrColor(cor)
+	canvas:attrFont('vera', tamanho)
+	canvas:drawText(x, y, texto)
 end
 
 -- Funcao de redesenho:
 -- chamada a cada tecla pressionada
 function redraw()
-	canvas:attrColor('black')
-	canvas:drawRect('fill', 0, 0, canvas:attrSize())
-	
-	-- canvas:attrColor('white')
-	-- canvas:attrFont('vera', 50)
-	-- canvas:drawText(50, 0, 'Você venceu!')
-	gerarObjetos()
-	for i = 1, #obstaculos do
-		canvas:compose(obstaculos[i].x, obstaculos[i].y, obstaculos[i].img)
+	if fase == #cordenadas_obstaculos then
+		gerarTexto('Você venceu!', 30, 'white', 100, 50)
+		-- gerarTexto('Pressione R para reiniciar', 30, 'white', 0, 30)
+		-- gerarTexto('Pressione ESC para sair', 30, 'white', 0, 60)
+	else
+		-- Cor do background
+		canvas:attrColor('black')
+		canvas:drawRect('fill', 0, 0, canvas:attrSize())
+
+		-- Gera os objetos jogador, chegada e obstáculos
+		gerarObjetos()
+
+		-- Gera o texto informando a fase atual
+		gerarTexto('Fase ' .. fase, 20, 'white', 0, 0)
 	end
 
 	canvas:flush()
 end
 
+
+
+
+-- Função para verificar a colisão entre objetos
 function collide(A, B)
 	return A.x < B.x + B.dx and A.x + A.dx > B.x and A.y < B.y + B.dy and A.y + A.dy > B.y
 end
+
+
+
+
 
 local IGNORE = false
 
@@ -135,6 +260,7 @@ function handler(evt)
 		return
 	end
 
+	print("teste")
 	-- apenas eventos de tecla me interessam
 	if evt.class == 'key' and evt.type == 'press'
 	then
@@ -163,13 +289,13 @@ function handler(evt)
 		-- Testa as colisões
 		if collide(jogador, chegada) then
 			fase = fase + 1
-			jogador.x = 0
-			jogador.y = 0
+			-- Atualiza as cordenadas do jogador e da chegada
+			atualizarCordenadas()
 		else
 			for i = 1, #obstaculos do
 				if collide(jogador, obstaculos[i]) then
-					jogador.x = 0
-					jogador.y = 0
+					jogador.x = cordenadas_jogador[fase][1]*bloco
+					jogador.y = cordenadas_jogador[fase][2]*bloco
 				end
 			end
 		end
